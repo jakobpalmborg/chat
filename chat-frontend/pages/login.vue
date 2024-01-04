35136b2f15ae218ee5292c5344466d4cb4866a9c
Jakob Palmborg <jakob.palmborg@gmail.com>
2024-01-04 15:36:38 +0100

    basic layout for login

--- chat-frontend/pages/login.vue
+++ chat-frontend/pages/login.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>this is the login</div>
+    <div>
+        <h1>Login</h1>
+        <form action="" class="flex flex-col ">
+            <label for="username">User Name:</label>
+            <input type="text" id="username" class="bg-slate-200">
+            <label for="password">Password:</label>
+            <input type="password" class="bg-slate-200">    
+        </form>
+     
+        <p class="mt-5">If you do not have an account, register here:</p>
+        <h1>Create Account</h1>
+        <form action="" class="flex flex-col ">
+            <label for="username">User Name:</label>
+            <input type="text" id="username" class="bg-slate-200">
+            <label for="email">Email:</label>
+            <input type="email" class="bg-slate-200">
+            <label for="password">Password:</label>
+            <input type="password" class="bg-slate-200">    
+        </form>
+
+    </div>
 </template>
