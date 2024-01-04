ca3971537a4d0c2ffe22423d6fcaf177c5779465
Jakob Palmborg <jakob.palmborg@gmail.com>
2024-01-04 15:44:32 +0100

    add btn

--- chat-frontend/pages/login.vue
+++ chat-frontend/pages/login.vue
@@ -5,7 +5,8 @@
             <label for="username">User Name:</label>
             <input type="text" id="username" class="bg-slate-200">
             <label for="password">Password:</label>
-            <input type="password" class="bg-slate-200">    
+            <input type="password" class="bg-slate-200 mb-1">   
+            <Btn /> 
         </form>
      
         <p class="mt-5">If you do not have an account, register here:</p>
@@ -16,7 +17,8 @@
             <label for="email">Email:</label>
             <input type="email" class="bg-slate-200">
             <label for="password">Password:</label>
-            <input type="password" class="bg-slate-200">    
+            <input type="password" class="bg-slate-200 mb-1">   
+            <Btn />   
         </form>
 
     </div>
