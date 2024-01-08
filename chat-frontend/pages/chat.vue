0b2044864e7ddd66e68ea7117a7c75cb53a8d1d7
Jakob Palmborg <jakob.palmborg@gmail.com>
2024-01-08 16:58:52 +0100

    render chat page if user logged in, else login and register links

--- chat-frontend/pages/chat.vue
+++ chat-frontend/pages/chat.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
-    <h1>{{ user }}, lets start chat</h1>
-    <form class="flex flex-col" action="">
-      <label for="room">Room:</label>
-      <input type="text" id="room" class="bg-slate-200 mb-1" />
-      <Btn />
-      <div></div>
-    </form>
+    <div v-if="user == ''">
+      <p class="block">
+        <nuxt-link to="/login">LOGIN</nuxt-link> or
+        <nuxt-link to="/register">REGISTER</nuxt-link> an account to start chat.
+      </p>
+    </div>
 
-    <h1>Chat room name</h1>
-    <ul>
-      <li>chat message</li>
-      <li>chat message</li>
-      <li>chat message</li>
-    </ul>
-    <form action="" class="flex flex-col">
-      <textarea
-        name="textMsg"
-        id="textMsg"
-        cols="45"
-        rows="3"
-        class="bg-slate-200 mb-1"
-        placeholder="start typing..."
-      ></textarea>
-      <Btn />
-    </form>
+    <div v-else>
+      <h1>{{ user }}, lets start chat</h1>
+      <form class="flex flex-col" action="">
+        <label for="room">Room:</label>
+        <input type="text" id="room" class="bg-slate-200 mb-1" />
+        <Btn />
+        <div></div>
+      </form>
+
+      <h1>Chat room name</h1>
+      <ul>
+        <li>chat message</li>
+        <li>chat message</li>
+        <li>chat message</li>
+      </ul>
+      <form action="" class="flex flex-col">
+        <textarea
+          name="textMsg"
+          id="textMsg"
+          cols="45"
+          rows="3"
+          class="bg-slate-200 mb-1"
+          placeholder="start typing..."
+        ></textarea>
+        <Btn />
+      </form>
+    </div>
   </div>
 </template>
 
