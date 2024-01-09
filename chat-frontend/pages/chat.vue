0d8dcf4339ce31adb4bc4e66c4f6d0664dcf0bbe
Jakob Palmborg <jakob.palmborg@gmail.com>
2024-01-09 09:24:24 +0100

    auth for fetching messages from strapi backend

--- chat-frontend/pages/chat.vue
+++ chat-frontend/pages/chat.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="user == ''">
+    <div v-if="user == '' || user == undefined">
       <p class="block">
         <nuxt-link to="/login">LOGIN</nuxt-link> or
         <nuxt-link to="/register">REGISTER</nuxt-link> an account to start chat.
@@ -34,9 +34,35 @@
         <Btn />
       </form>
     </div>
+    <h3 class="mt-10">get messages:</h3>
+    <Btn @click="getMessage()"></Btn>
+    <div>{{ msg }}</div>
   </div>
 </template>
 
 <script setup>
 const user = useCookie("user");
+const token = useCookie("token");
+const msg = ref("");
+
+const headers = new Headers({
+  Authorization: `Bearer ${token.value}`,
+});
+
+async function getMessage() {
+  try {
+    const response = await fetch("http://localhost:1337/api/messages", {
+      headers,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    msg.value = data.data;
+  } catch (e) {
+    console.log("Something went wrong with the fetch call!");
+    msg.value = "You have to login to get messages";
+    console.log(e);
+  }
+}
 </script>
