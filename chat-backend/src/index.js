eb3e9352443093ec25dc05c51652ad725eb4063f
Jakob Palmborg <jakob.palmborg@gmail.com>
2024-01-25 15:58:09 +0100

    change cors settings

--- chat-backend/src/index.js
+++ chat-backend/src/index.js
@@ -33,7 +33,7 @@ module.exports = {
     // @ts-ignore
     var io = require("socket.io")(strapi.server.httpServer, {
       cors: {
-        origin: process.env.NODE_ENV === "production" ? ["https://chat-sand-pi.vercel.app/"] : ["http://localhost:3000", "http://127.0.0.1:3000"]
+        origin: "https://chat-sand-pi.vercel.app/"
       }
     });
 
