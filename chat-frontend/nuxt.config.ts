f5358783e0c7060bd7fdbec0f560eb4d67483fe0
Jakob Palmborg <jakob.palmborg@gmail.com>
2024-02-18 21:45:15 +0100

    add hotjar

--- chat-frontend/nuxt.config.ts
+++ chat-frontend/nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  plugins: [{src: '~/plugins/hotjar.js', mode: 'client'}],
   postcss: {
     plugins: {
       tailwindcss: {},
